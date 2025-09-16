29489d07b09337fc4d8831c2e4cac00125f07ecf
alexander-russell <alexander.russell4@pm.me>
2025-09-17 09:51:07 +1000

    Add functions

---
+++ R/say.R
@@ -0,0 +1,32 @@
+#' Summmon a whale for a customised pick-me-up
+#'
+#' @param what The phrase the whale will utter
+#' @usage say(what)
+#' @returns Whale shaped message
+#' @export
+#'
+#' @examples
+#' say("You're whale-come")
+say <- function(what){
+  #Defining the whale ASCII
+  whale = "\n            ------ \n           %s \n            ------ \n               \\\   \n                \\\  \n                 \\\
+     .-'
+'--./ /     _.---.
+'-,  (__..-`       \\
+   \\          .     |
+    `,.__.   ,__.--/
+     '._/_.'___.-`
+"
+
+  #If what isn't supplied...
+  if(missing(what)){
+    what <- phrases %>% sample(size = 1)
+  }
+
+  #Combining message and whale together with sprintf ("%s" in the whale ASCII is replaced by "what")
+  out <- sprintf(whale, #Speech bubble
+                 what) #User specified message
+
+  #Return whale message
+  message(out)
+}
